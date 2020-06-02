b893c935e6e9223264bbf3ad56a7701a30119295
Djeckkenni <djeck27kenni@gmail.com>
2020-06-02 11:50:20 +0300

    correct code

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,21 +1,9 @@
-import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CalculatorTest {
     private Calculator calculator;
-
-    @BeforeClass
-    public static void beforeClass() {
-        System.out.println("Start test!");
-    }
-
-    @AfterClass
-    public static void afterClass() {
-        System.out.println("Stop test!");
-    }
 
     @Before
     public void initTest() {
@@ -24,37 +12,32 @@ public class CalculatorTest {
 
     @Test
     public void sumTest() {
-        Calculator calculator = new Calculator();
-        Assert.assertEquals(5, calculator.sum('+').operation(2, 3));
+        Assert.assertEquals(5, calculator.sum().operation(2, 3));
     }
 
     @Test
     public void subTest() {
-        Calculator calculator = new Calculator();
-        Assert.assertEquals(2, calculator.sub('-').operation(4, 2));
+        Assert.assertEquals(2, calculator.sub().operation(4, 2));
     }
 
     @Test
     public void multiplyTest() {
-        Calculator calculator = new Calculator();
-        Assert.assertEquals(12, calculator.multiply('*').operation(6, 2));
+        Assert.assertEquals(12, calculator.multiply().operation(6, 2));
     }
 
     @Test
     public void divideTest() {
-        Calculator calculator = new Calculator();
-        Assert.assertEquals(3, calculator.divide('/').operation(6, 2));
+        Assert.assertEquals(3, calculator.divide().operation(6, 2));
     }
 
     @Test(expected = ArithmeticException.class)
     public void divisionWithException() {
         Calculator calculator = new Calculator();
-        calculator.divide('/').operation(9, 0);
+        calculator.divide().operation(9, 0);
     }
 
     @Test
     public void exponentTest() {
-        Calculator calculator = new Calculator();
-        Assert.assertEquals(1 / 4, calculator.exponent('^').operation(2, -2));
+        Assert.assertEquals(1 / 4, calculator.exponent().operation(2, -2));
     }
 }
